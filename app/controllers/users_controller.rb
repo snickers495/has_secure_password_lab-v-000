b3484b9d71ddae6a9e16f1ab895c35dc1fa358e8
Lisa Lee <snickers495@gmail.com>
2017-12-09 16:09:53 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,6 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     binding.pry
     if @user.authenticate(params[:password])
-      
       @user.save
       session[:user_id] = @user.id
       redirect_to user_path
