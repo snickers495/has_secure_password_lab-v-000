c0ea242d1dcb9953f4743ad917db28727681b01c
Lisa Lee <snickers495@gmail.com>
2017-12-09 15:44:38 +0000

    automatically backed up by learn

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,13 +16,13 @@ RSpec.describe User, type: :model do
   it 'is valid if password and password_confirmation match' do
     user = User.new
     user.password = user.password_confirmation = 'foo'
-    expect(user.valid?).to be true    
+    expect(user.valid?).to be true
   end
 
   it 'is valid if password is set and password_confirmation is nil' do
      user = User.new
      user.password = 'foo'
-     expect(user.valid?).to be true 
+     expect(user.valid?).to be true
   end
 
   it "is invalid if password and password_confirmation are both non-nil and don't match" do
@@ -42,7 +42,7 @@ RSpec.describe User, type: :model do
     it "returns falsey if credentials don't match" do
       user = User.new
       user.password = 'foo'
-      expect(user.authenticate('fo0')).to be_falsey 
+      expect(user.authenticate('fo0')).to be_falsey
     end
   end
 end
