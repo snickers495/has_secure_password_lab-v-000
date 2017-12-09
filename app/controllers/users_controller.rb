1fe2d16af4667adb0005f554408e031e563b6bb5
Lisa Lee <snickers495@gmail.com>
2017-12-09 16:13:58 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     binding.pry
-    if @user.authenticate(params[:password])
+    if @user.authenticate(user_params[:password])
       @user.save
       session[:user_id] = @user.id
       redirect_to user_path
@@ -17,5 +17,4 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :password, :password_confirmation)
   end
-
 end
