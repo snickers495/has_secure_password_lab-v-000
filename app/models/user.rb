4274f252ca1fff7b9f28d84614344f211f447bb1
Lisa Lee <snickers495@gmail.com>
2017-12-09 16:00:26 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,10 @@ class User < ActiveRecord::Base
   def create
     user = User.new(user_params)
     if user.authenticate(params[:password]).save
-      session[:user_id] = user.id 
+      session[:user_id] = user.id
       redirect_to user_path
-    else 
-      redirect_to new_user_path 
+    else
+      redirect_to new_user_path
     end
 
   end
