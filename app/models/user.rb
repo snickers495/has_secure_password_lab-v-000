de31b3930c3319f4d573e2e00f4295eb166841be
Lisa Lee <snickers495@gmail.com>
2017-12-09 16:00:56 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
 
-  def create
-    user = User.new(user_params)
-    if user.authenticate(params[:password]).save
-      session[:user_id] = user.id
-      redirect_to user_path
-    else
-      redirect_to new_user_path
-    end
-
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:name, :password, :password_confirmation)
-  end
+  
 
 end
