995b40e534da65a18b1edfb008e7205a6a2cd67b
Lisa Lee <snickers495@gmail.com>
2017-12-09 15:49:08 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ActiveRecord::Base
   has_secure_password
 
+  def create
+    user = User.new(user_params).save
+  end
+ 
+  private
+ 
+  def user_params
+    params.require(:user).permit(:username, :password, :password_confirmation)
+  end
+
 end
