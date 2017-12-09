a21efe4d9a58df1b2d6bb57a83e0e8984169d566
Lisa Lee <snickers495@gmail.com>
2017-12-09 16:09:31 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,6 @@ class UsersController < ApplicationController
       binding.pry
       @user.save
       session[:user_id] = @user.id
-      
       redirect_to user_path
     else
       redirect_to new_user_path
