e8942c30040630cb078b971a69e500678e2c31ae
Lisa Lee <snickers495@gmail.com>
2017-12-09 16:01:05 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,19 @@
 class UsersController < ApplicationController
+  def create
+    user = User.new(user_params)
+    if user.authenticate(params[:password]).save
+      session[:user_id] = user.id
+      redirect_to user_path
+    else
+      redirect_to new_user_path
+    end
 
+  end
 
-end 
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  end
+
+end
