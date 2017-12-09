bb36abb5a36f92b1acd83f8859f7dec54e47c5eb
Lisa Lee <snickers495@gmail.com>
2017-12-09 15:44:33 +0000

    automatically backed up by learn

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe User, type: :model do
   it 'has a password field' do
     expect(User.new).to respond_to(:password)
   end
-
+ 
   it 'has a name field' do
     expect(User.new).to respond_to(:name)
   end
